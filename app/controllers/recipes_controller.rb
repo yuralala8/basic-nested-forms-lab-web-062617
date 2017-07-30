c5e71dd992e1b788f7436b51c01606541216450c
yuralala8 <yc1435@nyu.edu>
2017-07-30 17:47:11 -0400

    basic-nested-forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,16 @@
 class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build(recipe_id: '0')
+    @recipe.ingredients.build(recipe_id: '1')
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -7,10 +19,16 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  def new
-    @recipe = Recipe.new
-  end
 
-  def create
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :name,
+      :quantity
+    ]
+    )
   end
 end
